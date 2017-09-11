2284deedc20bc9ff73654caca4c2ebe29875cfb4
Jose Carlos Esteban <jc.esteban.gomez@gmail.com>
2017-09-11 23:52:08 +0200

    Ejemplo Mysql

---
+++ ejemplos/ejemplo_mysql/index.js
@@ -0,0 +1,30 @@
+"use strict"
+
+//cargamos el driver
+const mysql = require('mysql');
+
+//crear una conexión
+const connection = mysql.createConnection({
+    host: 'didimo.es',
+    user: 'usuariocurso',
+    password: 'us3r',
+    database: 'cursonode'
+});
+
+//conectar
+connection.connect();
+
+//lanzar una query
+connection.query('SELECT * FROM agentes',(err, rows, fields)=>{
+    if (err){
+        console.log('Hubo un error', err);
+        process.exit(1);
+        return;
+    }
+    for (let i= 0; i < rows.length; i++){
+        const agente = rows[i];
+        console.log(agente.idagentes, agente.name, agente.age);
+    }
+});
+
+//pintar los resultados
